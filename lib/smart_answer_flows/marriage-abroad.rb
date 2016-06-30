9152c9f1c4dfc919bcf95e7b8c1558e7c47c1498
Chris Roos <chris@seagul.co.uk>
2016-06-30 15:26:13 +1000

    Add outcome for opposite sex ceremonies in Egypt
    This new template is a copy of
    outcome_opposite_sex_marriage_in_affirmation_countries.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -203,7 +203,7 @@ module SmartAnswer
             elsif calculator.ceremony_country == 'turkey'
               outcome :outcome_opposite_sex_marriage_in_turkey
             elsif calculator.ceremony_country == 'egypt'
-              outcome :outcome_opposite_sex_marriage_in_affirmation_countries
+              outcome :outcome_opposite_sex_marriage_in_egypt
             elsif calculator.opposite_sex_affirmation_country?
               outcome :outcome_opposite_sex_marriage_in_affirmation_countries
             elsif calculator.ceremony_country_in_the_commonwealth? ||
@@ -291,6 +291,7 @@ module SmartAnswer
       outcome :outcome_opposite_sex_marriage_in_consular_cni_countries_when_residing_in_uk_or_ceremony_country
       outcome :outcome_opposite_sex_marriage_in_dominican_republic
       outcome :outcome_opposite_sex_marriage_in_ecuador
+      outcome :outcome_opposite_sex_marriage_in_egypt
       outcome :outcome_opposite_sex_marriage_in_germany
       outcome :outcome_opposite_sex_marriage_in_georgia
       outcome :outcome_opposite_sex_marriage_in_hong_kong
